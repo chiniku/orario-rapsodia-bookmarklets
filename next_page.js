2b5c7141337f221fde665bdd62e325bed2c5b6cd
chiniku <43088308+chiniku@users.noreply.github.com>
2018-10-09 00:45:44 +0900

    Create next_page.js

---
+++ next_page.js
@@ -0,0 +1,23 @@
+javascript:
+(function() {
+  'use strict';
+
+  const page_selected = game_frame().querySelector('#asyPager > ul > li > a.selected');
+  if (!page_selected) {
+    console.log('no pager');
+    return;
+  }
+
+  const next_page =  page_selected.parentNode.nextElementSibling;
+  if (!next_page) {
+    console.log('no next_page');
+    return;
+  }
+
+  next_page.querySelector('a.btn_pager01').click();
+
+  function game_frame(){
+    return document.querySelector('#game_frame').contentDocument;
+  }
+
+})();
