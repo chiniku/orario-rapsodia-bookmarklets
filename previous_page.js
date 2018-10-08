8d6bb786fd07e771dd0dc4b263b7639040d5b3cd
chiniku <43088308+chiniku@users.noreply.github.com>
2018-10-09 00:46:09 +0900

    Create previous_page.js

---
+++ previous_page.js
@@ -0,0 +1,23 @@
+javascript:
+(function() {
+  'use strict';
+
+  const page_selected = game_frame().querySelector('#asyPager > ul > li > a.selected');
+  if (!page_selected) {
+    console.log('no pager');
+    return;
+  }
+
+  const previous_page =  page_selected.parentNode.previousElementSibling;
+  if (!previous_page) {
+    console.log('no previous_page');
+    return;
+  }
+
+  previous_page.querySelector('a.btn_pager01').click();
+
+  function game_frame(){
+    return document.querySelector('#game_frame').contentDocument;
+  }
+
+})();
