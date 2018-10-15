7dbe9927ec09e87e4910225a1b0d9b1fa560a52c
chiniku <43088308+chiniku@users.noreply.github.com>
2018-10-15 22:06:09 +0900

    Update generate_weapon_bookmarklet.sh
    Support event weapon

--- generate_weapon_bookmarklet.sh
+++ generate_weapon_bookmarklet.sh
@@ -2,14 +2,30 @@
 
 set -u -e -o pipefail
 
-usage="Usage: ./generate_weapon_bookmarklet.sh <weapon_type> <weapon_name>"
+usage="Usage: ./generate_weapon_bookmarklet.sh [-e] <weapon_type> <weapon_name>"
 
 show_help() {
 cat << EOF
 $usage
 
+  Generate bookmarklet to create a weapon
+      -h    Display this help and exit.
+      -e    Event weapon flag
+
 EOF
 }
+# Default
+event_weapon=false
+
+# Parse options
+OPTIND=1
+while getopts "he" opt; do
+    case "$opt" in
+        "h") show_help; exit 0;;
+        "e") event_weapon=true;;
+    esac
+done
+shift $(($OPTIND - 1))
 
 if [ $# -lt 2 ]; then
     show_help
@@ -20,5 +36,11 @@ weapon_type="$1"
 weapon_name="$2"
 weapon_type_replace_string=$(printf "s/種類/%s/" "$weapon_type")
 weapon_name_replace_string=$(printf "s/名前/%s/" "$weapon_name")
+event_flag_replace_string=$(printf "s/const event_weapon = false;/const event_weapon = %s;/" "$event_weapon")
 
-cat create_weapon.js | sed -E "$weapon_type_replace_string" | sed -E "$weapon_name_replace_string"
+cat create_weapon.js \
+    | sed -E "$weapon_type_replace_string" \
+    | sed -E "$weapon_name_replace_string" \
+    | sed -E "$event_flag_replace_string"
+
+
